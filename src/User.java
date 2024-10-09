09392c07fb0a08e8532ae2011a94147b904c769d
Breno Lemos <brenolemos327@gmail.com>
2024-10-09 19:08:59 -0300

    Commit Test

--- src/User.java
+++ src/User.java
@@ -11,38 +11,6 @@ public class User {
         this.name = name;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    private void setEmail(String email) {
-        this.email = email;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    private void setHeight(double height) {
-        this.height = height;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    private void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    private void setAge(int age) {
-        this.age = age;
-    }
-
     @Override
     public String toString() {
         return name + "\n" + email + "\n" + age + "\n" + height;
